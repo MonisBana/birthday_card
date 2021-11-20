baca271ee95c7b26aa60fca2e0d3449018d83c12
Monis Bana <monisbana97@gmail.com>
2021-11-20 18:08:47 +0530

    Cake changed and wishes added

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./Card.scss"
 import { isMobile } from 'react-device-detect';
+import Cake from '../Cake/Cake';
 
 const Card = () => {
     const [toggle, setToggle] = useState(false)
@@ -12,7 +13,8 @@ const Card = () => {
         <div className="scene" onClick={() => setToggle(!toggle)}>
             <div className={cardStyle}>
                 <div className="card__face card__face--front" style={{ display: toggle ? 'none' : null }}>
-                    <div className="cover-shape-large">
+                    <Cake />
+                    {/* <div className="cover-shape-large">
                         <div className="shape-diamond"></div>
                         <div className="shape-block"></div>
                     </div>
@@ -31,14 +33,15 @@ const Card = () => {
                                     <div className="flame"></div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+
+                    </div> 
+                </div>*/}
                 </div>
                 <div className="card__face card__face--back" style={{ display: !toggle ? 'none' : null }}>
                     <div className="text-container">
-                        <p id="head">Happy Birthday Mom!</p>
-                        <p>Your first digital birthday card! <span className="strikethrough">Handcrafted</span> Handcoded from scratch just for you.</p>
-                        <p>Hope your day goes great!</p>
+                        <p id="head">Happy Birthday Aliza!</p>
+                        <p>I wish you have a great day and a great year ahead.<br />I love you more than you can imagine.</p>
+                        <p>May this day be as special as you are to me!</p>
                     </div>
                 </div>
             </div>
