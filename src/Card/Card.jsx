d61d67dcfb0bf67984b89d658c94fb83d65985ae
Monis Bana <monisbana97@gmail.com>
2021-11-21 11:25:47 +0530

    Re render svg

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -13,29 +13,7 @@ const Card = () => {
         <div className="scene" onClick={() => setToggle(!toggle)}>
             <div className={cardStyle}>
                 <div className="card__face card__face--front" style={{ display: toggle ? 'none' : null }}>
-                    <Cake />
-                    {/* <div className="cover-shape-large">
-                        <div className="shape-diamond"></div>
-                        <div className="shape-block"></div>
-                    </div>
-                    <div className="cover-shape-small">
-                        <div className="shape-diamond"></div>
-                        <div className="shape-block">
-                            <div className="cake">
-                                <div className="layer layer-bottom"></div>
-                                <div className="layer layer-middle"></div>
-                                <div className="layer layer-top"></div>
-                                <div className="icing"></div>
-                                <div className="drip drip1"></div>
-                                <div className="drip drip2"></div>
-                                <div className="drip drip3"></div>
-                                <div className="candle">
-                                    <div className="flame"></div>
-                                </div>
-                            </div>
-
-                    </div> 
-                </div>*/}
+                    <Cake toggle={toggle} />
                 </div>
                 <div className="card__face card__face--back" style={{ display: !toggle ? 'none' : null }}>
                     <div className="text-container">
